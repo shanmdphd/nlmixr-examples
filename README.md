5834f9917c9b626d5347ce4c7003f1346c05abae
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-04-02 15:41:47 -0500

    Fix spelling

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Example models for nlmixr
 
 - Case Study 1: [Nimotuzumab (by Mirjam Trame)](case-study-nimotuzumab/Nimo.R)
-- Case Study 2: [Mavoglurant PBPK (by Wenping Wang)](case-sutdy-mavoglurant/PBPK.R)
+- Case Study 2: [Mavoglurant PBPK (by Wenping Wang)](case-study-mavoglurant/PBPK.R)
 - Multiple Endpoint 
   - [Warfarin PKPD](case-study-warfarin/warfarin_PKPD_joint.R) 
--- case-study-warfarin/warfarin_PKPD_joint.R
+++ case-study-warfarin/warfarin_PKPD_joint.R
@@ -16,7 +16,6 @@ data.pkpd <- read.csv(file = paste(homedir,"warfarin_dat.csv",sep="/"))
 ## directly. by data.pkpd$dvid + 2
 ##
 data.pkpd$cmt  <- factor(data.pkpd$dvid,c(1,2),c("center","effect"))
-
 pk.turnover.emax <- function() {
   ini({
     tktr <- log(0.0001)
@@ -90,3 +89,65 @@ pdf("warfarin-joint-focei.pdf");
 plot(fit.TOF)
 vpc.ui(fit.TOF, stratify="CMT", show=list(obs_dv=T))
 dev.off();
+
+
+pk.turnover.emax <- function() {
+  ini({
+    tktr <-  fix(-5.99)
+    tka <- fix(0.801)
+    tcl <- fix(-2.04)
+    tv <- fix(2.07)
+
+    eta.ktr ~ fix(2.688646)
+    eta.ka ~ fix(0.8642011)
+    eta.cl ~ fix(0.06563665)
+    eta.v ~ fix(0.03526201)
+    prop.err <- fix(0.198)
+    pkadd.err <- fix(0.00271)
+
+    poplogit <- 2
+    #temax <- 7.5
+    tec50 <- log(0.5)
+    tkout <- log(0.05)
+    te0 <- log(100)
+
+    eta.emax ~ .5
+    eta.ec50  ~ .5
+    eta.kout ~ .5
+    eta.e0 ~ .5
+
+    pdadd.err <- 4
+  })
+  model({
+    ktr <- exp(tktr + eta.ktr)
+    ka <- exp(tka + eta.ka)
+    cl <- exp(tcl + eta.cl)
+    v <- exp(tv + eta.v)
+
+    #poplogit = log(temax/(1-temax))
+    logit=exp(poplogit+eta.emax)
+    #logit=temax+eta.emax
+    emax = logit/(1+logit)
+    ec50 =  exp(tec50 + eta.ec50)
+    kout = exp(tkout + eta.kout)
+    e0 = exp(te0 + eta.e0)
+
+    DCP = center/v
+    PD=1-emax*DCP/(ec50+DCP)
+
+    effect(0) = e0
+    kin = e0*kout
+
+    d/dt(depot) = -ktr * depot
+    d/dt(gut) =  ktr * depot -ka * gut
+    d/dt(center) =  ka * gut - cl / v * center
+    d/dt(effect) = kin*PD -kout*effect
+
+    cp = center / v
+    cp ~ prop(prop.err) + add(pkadd.err) | center
+    effect ~ add(pdadd.err) | effect
+  })
+}
+
+
+fit.TOF <- nlmixr(pk.turnover.emax, data.pkpd, est="focei")
